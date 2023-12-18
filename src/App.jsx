841e65cedbe5678307ffb70f03c18c2532170721
Emilse Arias <emilsearias@gmail.com>
2023-12-18 17:34:10 -0300

    agregamos la libreria de bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
- 
+// aqui agregamos la logica del componente
+
   return (
-    <h1>Prueba</h1>
+    // aqui puedo agregar un poquito mas de logica
+    <h1 className='text-center display-3'>
+      {/* aqui agrego todo el maquetado del componente */}
+      Prueba</h1>
   )
 }
 
