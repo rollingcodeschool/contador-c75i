60f251ddc5e746d08b8742ef383c57720b56b04d
Emilse Arias <emilsearias@gmail.com>
2023-12-18 17:55:31 -0300

    agregamos el maquetado del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-// aqui agregamos la logica del componente
+  // aqui agregamos la logica del componente
 
   return (
     // aqui puedo agregar un poquito mas de logica
-    <h1 className='text-center display-3'>
-      {/* aqui agrego todo el maquetado del componente */}
-      Prueba</h1>
-  )
+    <section className="container my-4">
+      <h1 className="text-center display-3">
+        {/* aqui agrego todo el maquetado del componente */}
+        Contador
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+        voluptate totam natus eum iste illo excepturi tempora placeat nesciunt
+        eos?
+      </p>
+    </section>
+  );
 }
 
-export default App
+export default App;
